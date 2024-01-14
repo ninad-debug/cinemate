7cdf68c45924219bb59dc2696cf9aae612b133ec
ninad-debug <ninadsamant07@gmail.com>
2024-01-14 09:58:32 +0530

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { Header } from "./Header";
 export { Footer } from "./Footer";
 
 export { Card } from "./Card";
-export {Button} from './Button';
+export { Button } from "./Button";
 
-export {ScrollToTop} from "./ScrollToTop";
+export { ScrollToTop } from "./ScrollToTop";
+export { Cast } from "./Cast";
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,20 +1,21 @@
-import { Card } from "../components/index"
+import { Card } from "../components/index";
 import { useFetch } from "../hooks/useFetch";
 import { useTitle } from "../hooks/useTitle";
 
-export const MovieList = ({apiPath, title}) => {
-  const {data : movies} = useFetch(apiPath);
+export const MovieList = ({ apiPath, title }) => {
+  const { data: movies } = useFetch(apiPath);
+  console.log(movies);
   useTitle(title);
 
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap other:justify-evenly">
-          {movies.map((movie)=>(
-            <Card key={movie.id} movie={movie}/>
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
           ))}
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
